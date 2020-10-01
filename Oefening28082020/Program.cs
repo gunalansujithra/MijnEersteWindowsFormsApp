59c3c8ca82fd8948b04c3c2d0c53a6c30904252c
gunalansujithra@gmail.com <gunalansujithra@gmail.com>
2020-10-01 15:36:12 +0200

    Boeken

---
+++ Oefening28082020/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening28082020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //------------------------------------------------
+            //Oefening 2
+            //------------------------------------------------
+
+            
+        }
+    }
+}
